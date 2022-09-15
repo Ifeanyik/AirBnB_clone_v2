78c232946cf4a122fe6b28eda681b1dfef6a0f16
Ifeanyik <jax64720@gmail.com>
2022-09-15 14:12:50 -0700

    Fixing errors

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,13 @@ from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -55,4 +57,6 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        """
+        """Delete current instance from storage"""
+        from models import storage
+        storage.delete(self)
