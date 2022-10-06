4a8c34f39a8b661c3a4f6a2e4c8b228be5bb7dea
Ifeanyik <jax64720@gmail.com>
2022-10-06 07:12:15 -0700

    Fixing errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,13 @@
 #!/usr/bin/env bash
-# this script sets up web servers for deployment of web_static
-if ! command -v nginx &> /dev/null
-then
-	sudo apt-get -y update
-	sudo apt-get -y install nginx
-	sudo service nginx start
-fi
-if [ ! -d '/data/' ]
-then
-	sudo mkdir - p '/data/'
-fi
-if [ ! -d '/data/web_static/' ]
-then
-	sudo mkdir -p '/data/web_static'
-fi
-if [ ! -d '/data/web_static/releases/' ]
-then
-	sudo mkdir -p '/data/web_static/releases/'
-fi
-if [ ! -d '/data/web_static/shared/' ]
-then
-	sudo mkdir -p '/data/web_static/shared/'
-fi
-if [ ! -d '/data/web_static/releases/test/' ]
-then
-	sudo mkdir -p '/data/web_static/releases/test/'
-fi
-echo -e '<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
-if [ -L ${/data/web_static/current} ]
-then
-	sudo rm -r /data/web_static/current
-	ln -s /data/web_static/releases/test/ /data/web_static/current
-else
-	ln  -s /data/web_static/releases/test/ /data/web_static/current
-fi
+# Script that configures Nginx server with some folders and files
+sudo apt-get -y update
+sudoapt-get -y install nginx
+sudoservice nginx start
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Hello World!" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
 exit 0
