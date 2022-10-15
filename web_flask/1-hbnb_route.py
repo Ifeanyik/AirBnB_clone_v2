e50782f8dc32c78fc6cd204a55284e91a13b4fae
Ifeanyik <jax64720@gmail.com>
2022-10-15 13:41:22 -0700

    Blahhhh

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -19,6 +19,5 @@ def hbnb():
     return "HBNB"
 
 
-if __name__ == "main":
-    app.url_map.strict_slashes = False
-    app.run(host="0.0.0.0", port=5000)
+app.url_map.strict_slashes = False
+ app.run(host="0.0.0.0", port=5000)
