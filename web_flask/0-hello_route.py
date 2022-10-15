7fe7e9a866f71587ddc8961b405443c5ba89c455
Ifeanyik <jax64720@gmail.com>
2022-10-15 13:36:24 -0700

    Remove if block

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,6 +14,5 @@ def display():
     return "Hello HBNB!"
 
 
-if __name__ == 'main':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+app.url_map.strict_slashes = False
+app.run(host='0.0.0.0', port=5000)
