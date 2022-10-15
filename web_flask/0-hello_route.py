c0504f42135be1ecddf2ff830196d51b07efca79
Ifeanyik <jax64720@gmail.com>
2022-10-15 13:34:12 -0700

    Use single quotes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,13 +7,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def display():
     '''this function returns a string
     for any url starting with /'''
     return "Hello HBNB!"
 
 
-if __name__ == "main":
+if __name__ == 'main':
     app.url_map.strict_slashes = False
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
