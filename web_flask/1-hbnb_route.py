ad439b32cad71fb8f2c93d330b8ab2a5a9fba375
Ifeanyik <jax64720@gmail.com>
2022-10-15 08:48:52 -0700

    Conform to pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,16 +4,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def display():
     '''this function returns a string
     for any url starting with /'''
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     '''returns a string for any url
     starting with /hbnb'''
     return "HBNB"
+
 
 app.run(host="0.0.0.0", port=5000)
