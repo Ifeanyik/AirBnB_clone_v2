848a1112fb4c7c7746498377e16bf87100235205
Ifeanyik <jax64720@gmail.com>
2022-10-15 13:31:44 -0700

    Url_map

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,18 +5,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def display():
     '''this function returns a string
     for any url starting with /'''
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     '''returns a string for any url
     starting with /hbnb'''
     return "HBNB"
 
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "main":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
