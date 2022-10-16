2b8a5a847ebf1a06bbdae50add140356c01e1410
Ifeanyik <jax64720@gmail.com>
2022-10-16 00:44:14 -0700

    End

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,14 +8,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def display():
+def hello():
     '''this function returns a string
     for any url starting with /'''
     return "Hello HBNB!"
 
 if __name__ == "main":
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
-else:
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
