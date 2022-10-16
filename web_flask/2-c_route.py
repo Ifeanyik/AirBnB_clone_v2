8a43c16fb95e9e54295ac6f54bf0e9e235b71157
Ifeanyik <jax64720@gmail.com>
2022-10-16 01:23:51 -0700

    Match url /c/<variable_text>

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''this scripts starts a Flask web application'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def display():
+    '''this function returns a string
+    for any url starting with /'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    '''returns a string for any url
+    starting with /hbnb'''
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_what(text):
+    '''returns a streing for urls matching /c/'''
+    new_text = ""
+    for i in text:
+        if i == "_":
+            new_text += " "
+        else:
+            new_text += i
+    return "C {}".format(new_text)
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
